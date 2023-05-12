b78d7b67077236a7bc5e263771703d9e6615f64c
Somogyi Krisztian <krisztian@somogyi.xyz>
2023-05-12 11:52:56 +0200

    Add project files.

---
+++ BeforeAfterFodyINotifyPropChanged/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+
+namespace BeforeAfterFodyINotifyPropChanged;
+
+public class Animal : INotifyPropertyChanged
+{
+    public Animal()
+    {
+        PropertyChanged += delegate (object? sender, PropertyChangedEventArgs args)
+        {
+            Debug.WriteLine($"{args.PropertyName}");
+        };
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public string Name { get; set; }
+
+    public void OnPropertyChanged(string propertyName, object before, object after)
+    {
+        Debug.WriteLine($"{before}:{after}");
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+public static class PropertyChangedNotificationInterceptor
+{
+    public static void Intercept(object target, Action onPropertyChangedAction,
+        string propertyName, object before, object after)
+    {
+        onPropertyChangedAction();
+    }
+}
+
